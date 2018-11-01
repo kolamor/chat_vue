c738cbfe133ab1d8b6292e7c6a9ba2f653df8566
kolamor <kolamorev@mail.ru>
2018-11-01 16:34:07 +0300

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,54 @@
+<template>
+	<div>
+		<input v-model="login" type="text" placeholder="Логин"/>
+		<input v-model="password" type="password" placeholder="Пароль"/>
+		<button @click="setLogin">Войти</button>
+		</br>
+		{{login}} - {{password}}
+	</div>
+</template>
+
+<script>
+	import $ from 'jquery'
+
+	export default{
+		name: "Login",
+		data() {
+			return {
+				login:'',
+				password:'',
+			}
+		},
+		methods: {
+			setLogin() {
+			$.ajax({
+				url: "http://127.0.0.1:8000/auth/token/create/",
+				type: "POST",
+				data: {
+					username: this.login,
+					password: this.password
+				},
+				success: (response) => {
+					console.log(response.data.attributes.auth_token),
+					alert("Вход выполнен"),
+					sessionStorage.setItem("auth_token", response.data.attributes.auth_token)
+					
+				},
+				error: (response) => {
+					if (response.status === 400){
+						alert("логин или пароль не верен")
+					}
+					console.log(response)
+
+
+				}
+			})
+
+			},
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
